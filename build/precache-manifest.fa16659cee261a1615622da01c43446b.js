5ba72a0eb46559be3622aacf3c61133cf7cae3a1
chrpng <chrpng.brd@gmail.com>
2020-12-12 15:48:01 -0500

    implement pubsub pattern when books are added

---
+++ build/precache-manifest.fa16659cee261a1615622da01c43446b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263d5150aa87a3de25ce085041361a3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3701f28907ed8d1455",
+    "url": "/static/js/2.2c863a3a.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.2c863a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b38589abfbc844e1b9",
+    "url": "/static/js/main.008b62e2.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
